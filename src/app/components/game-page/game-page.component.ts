a097ed41c51494d878e8b54eb819c4cff10cc3af
roch10 <bajracharyaroch@gmail.com>
2019-02-08 11:43:00 +0545

    Game AI completed

--- src/app/components/game-page/game-page.component.ts
+++ src/app/components/game-page/game-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-game-page',
@@ -8,9 +9,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GamePageComponent implements OnInit {
   board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
+    ["0", "1", "2"],
+    ["3", "4", "5"],
+    ["6", "7", "8"]
   ];
   player: any;
   sqrId: any;
@@ -19,49 +20,71 @@ export class GamePageComponent implements OnInit {
   row: any;
   col: any;
   boardValues = {
-    "zero": "", "one": "", "two": "", "three": "", "four": "", "five": "", "six": "", "seven": "", "eight": "", "nine": "",
+    "zero": "", "one": "", "two": "", "three": "", "four": "", "five": "", "six": "", "seven": "", "eight": ""
   }
   isDraw = false;
+
 
   ARRAY_LENGHT = 3;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    @Inject(DOCUMENT) document
   ) { }
 
   ngOnInit() {
     this.user = this.route.snapshot.paramMap.get('player');
-    console.log("user", this.user);
+    console.log("board", this.board);
     this.player = this.user;
     this.computer = (this.user == "x") ? "o" : "x";
   }
 
   onClick(sqrName, event) {
     this.sqrId = event.target.attributes.id.value;
-    if (this.boardValues[sqrName] === "") {
-      this.boardValues[sqrName] = this.player;
+    this.userMove(sqrName);
+    var score = this.checkWinner(this.board);
+    if (score === -10) {
+      alert("You Won!!!");
+    } else if (this.checkDraw(this.board)) {
+      alert("It's a Draw!!!");
+      this.onReset();
+    } else {
+      setTimeout(() => {
+        this.computerMove();
+      }, 200);
+    }
+  }
+
+  userMove(spotValue) {
+    if (this.boardValues[spotValue] === "") {
+      this.boardValues[spotValue] = this.user;
       this.row = this.getRows(this.sqrId);
       this.col = this.getCol(this.sqrId);
-      console.log("player", this.player);
-      this.board[this.row][this.col] = this.player;
-      console.log(this.board);
-      this.computerMove();
-
-      if (!this.checkWinner()) {
-        this.checkDraw();
-      }
-      // this.turn = (this.turn == this.user) ? this.computer : this.user; for 2 players
+      this.board[this.row][this.col] = this.user;
     }
+  }
+
+  computerMove() {
+    var bestMove = this.getBestMove(this.board);
+    let store = this.board[bestMove.row][bestMove.col];
+    this.board[bestMove.row][bestMove.col] = this.computer;
+    this.boardValues[this.returnNumberName(store)] = this.computer;
+    var score = this.checkWinner(this.board);
+    setTimeout(() => {
+      if (score === +10) {
+        alert("You loose!!!");
+      }
+    }, 100);
   }
 
   onReset() {
     this.boardValues = {
-      "zero": "", "one": "", "two": "", "three": "", "four": "", "five": "", "six": "", "seven": "", "eight": "", "nine": "",
+      "zero": "", "one": "", "two": "", "three": "", "four": "", "five": "", "six": "", "seven": "", "eight": ""
     };
     this.board = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""]
+      ["0", "1", "2"],
+      ["3", "4", "5"],
+      ["6", "7", "8"]
     ];
   }
 
@@ -73,50 +96,55 @@ export class GamePageComponent implements OnInit {
     return id % this.ARRAY_LENGHT;
   }
 
-  checkWinner() {
+  checkWinner(board) {
     for (var i = 0; i < this.ARRAY_LENGHT; i++) {
-      if (this.board[i][0] !== "" && this.board[i][0] === this.board[i][1] && this.board[i][1] === this.board[i][2]) {    //for rows
-        alert("Game Complete!!!");
-        return true;
-      } else if (this.board[0][i] !== "" && this.board[0][i] === this.board[1][i] && this.board[1][i] === this.board[2][i]) {   //for coloms
-        alert("Game Complete!!!");
-        return true;
-      }
-    }
-
-    if (this.board[0][0] !== "" && this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2]) {
-      alert("Game Complete!!!");
-      return true;
-    } else if (this.board[0][2] !== "" && this.board[0][2] === this.board[1][1] && this.board[1][1] === this.board[2][0]) {
-      alert("Game Complete!!!");
-      return true;
-    }
-  }
-
-  checkDraw() {
-    for (var i = 0; i < this.ARRAY_LENGHT; i++) {
-      for (var j = 0; j < this.board[i].length; j++) {
-        if (this.board[i][j] === "") {
-          return;
+      if ((board[i][0] === "x" || board[i][0] === "o") && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {    //for rows
+        if (board[i][0] === this.computer) {
+          return +10;
+        } else if (board[i][0] === this.user) {
+          return -10;
+        }
+      } else if ((board[0][i] === "x" || board[0][i] === "o") && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {   //for coloms
+        if (board[0][i] === this.computer) {
+          return +10;
+        } else if (board[0][i] === this.user) {
+          return -10;
         }
       }
     }
-    alert("It is a draw!!");
+
+    if ((board[0][0] === "x" || board[0][0] === "o") && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+      if (board[0][0] === this.computer) {
+        return +10;
+      } else if (board[0][0] === this.user) {
+        return -10;
+      }
+    } else if ((board[0][2] === "x" || board[0][2] === "o") && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+      if (board[0][2] === this.computer) {
+        return +10;
+      } else if (board[0][2] === this.user) {
+        return -10;
+      }
+    }
+
+    return 0;
   }
 
-  computerMove() {
-    var random;
-    do {
-      var r = Math.floor(Math.random() * 8);
-      random = this.returnNumberName(r);
-    } while (this.boardValues[random] != "");
-    this.boardValues[random] = this.computer;
-    this.row = this.getRows(r);
-    this.col = this.getCol(r);
-    this.board[this.row][this.col] = this.computer;
+  checkDraw(board) {
+    for (var i = 0; i < this.ARRAY_LENGHT; i++) {
+      for (var j = 0; j < board[i].length; j++) {
+        if (board[i][j] !== "x" && board[i][j] !== "o") {
+          return false;
+        }
+      }
+    }
+    return true;
   }
+
+
 
   returnNumberName(num) {
+    num = +num;
     switch (num) {
       case 0:
         return "zero";
@@ -139,5 +167,89 @@ export class GamePageComponent implements OnInit {
       default:
         break;
     }
+  }
+
+  hasMoveLeft(board) {
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        if (board[i][j] !== "x" && board[i][j] !== "o")
+          return true;
+      }
+    }
+    return false;
+  }
+
+  minimax(board, depth, isComp: boolean) {
+    var score = this.checkWinner(board);
+
+    if (score === 10)
+      return score;
+
+    if (score === -10)
+      return score;
+
+    if (!this.hasMoveLeft(board))
+      return 0;
+
+    if (isComp) {
+      let best = -1000;
+
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          if (board[i][j] !== "x" && board[i][j] !== "o") {
+            let store = board[i][j];
+            board[i][j] = this.computer;
+            var minmaxValue = this.minimax(board, depth + 1, !isComp);
+            best = (best < minmaxValue) ? minmaxValue : best;
+
+            board[i][j] = store;
+          }
+        }
+      }
+      return best;
+    } else {
+      let best = 1000;
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          if (board[i][j] !== "x" && board[i][j] !== "o") {
+            let store = board[i][j];
+            board[i][j] = this.user;
+            var minmaxValue = this.minimax(board, depth + 1, !isComp);
+            best = (minmaxValue < best) ? minmaxValue : best;
+
+            board[i][j] = store;
+          }
+        }
+      }
+      return best;
+
+    }
+  }
+
+  getBestMove(board) {
+    var bestValue = -1000;
+    var bestMove = {
+      row: -1,
+      col: -1
+    }
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        if (board[i][j] !== "x" && board[i][j] !== "o") {
+          let store = board[i][j];
+          board[i][j] = this.computer;
+
+          var moveValue = this.minimax(board, 0, false);
+
+          board[i][j] = store;
+
+          if (moveValue > bestValue) {
+            bestMove.row = i;
+            bestMove.col = j;
+            bestValue = moveValue;
+          }
+        }
+      }
+    }
+    return bestMove;
   }
 }
